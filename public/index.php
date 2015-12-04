34f2249730ab95a273534737cf97116b1349fc9c
sbienvenu <sebastien.bienvenu@pharmagest.com>
2015-12-04 16:22:34 +0100

    qsQSqs

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This makes our life easier when dealing with paths. Everything is relative
+ * This makes our life easwxcsddsfsdfier when dealing with paths. Everything is relative
  * to the application root now.
  */
 echo "Welcome Sebastien";
